0d3be45ec52a729d0f8907805cc5e4714c95108e
WenChi <u10416019@go.utaipei.edu.tw>
2016-03-06 17:18:59 +0800

    Create some accessor and mutator
    This file create some accessor and mutator of the data field, such as return the numbers to calculate the answer, then return the answer, and also create a mutator of the arraylist to record the answers which the user enter.

--- RepeatAdditionQuiz.java
+++ RepeatAdditionQuiz.java
@@ -13,6 +13,25 @@ public class RepeatAdditionQuiz{
 	//create an empty constructor
 	RepeatAdditionQuiz(){
 	}
-
+		
+	//create a mutator of addAnswerList to input the answer of all the user input
+	void setAddAnswerList(int answer){
+		answerList.add(answer);
 	}
+		
+	//create a accessor to calculate and return the answer of the multiplication
+	public int getAnswerCheck(){
+		return (number1 * number2);
+	}
+	
+	//create a accessor of number1 to return number1
+	public int getNumber1(){
+		return number1;
+	}
+	
+	//create a accessor of number2 to return number2
+	public int getNumber2(){
+		return number2;
+	}
+
 }
