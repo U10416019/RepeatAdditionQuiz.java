f261a0d920e5e9de5c2ed67538d4298da008f177
WenChi <u10416019@go.utaipei.edu.tw>
2016-03-06 16:51:59 +0800

    Prototype of the multiplication quiz
    The file is the prototype of the multiplication quiz, it has some date fields to calculate the quiz, and it also has an empty constructor which could be called in main class.

---
+++ RepeatAdditionQuiz.java
@@ -0,0 +1,18 @@
+//U10416019鄭文琪
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+
+public class RepeatAdditionQuiz{
+	//data field
+	private SecureRandom random = new SecureRandom();
+	private int number1 = random.nextInt(10);
+	private int number2 = random.nextInt(10);
+	private ArrayList<Integer> answerList = new ArrayList<>();
+	
+	//create an empty constructor
+	RepeatAdditionQuiz(){
+	}
+
+	}
+}
