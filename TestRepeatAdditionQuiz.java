e7be90e179de8ae5455913082e7883d29bb4ef1d
WenChi <u10416019@go.utaipei.edu.tw>
2016-03-06 17:38:24 +0800

    Determine whether the user enter the true answer
    Create a while loop to check whether the user enter the true answer, if the user enter the wrong answer then into the loop and store the wrong answer in the arraylist, after that, check whether the wrong answer is enter the same before, then ask the user enter the answer again.  If the user enter the true answer, then print the message to tell the user that the answer is true.

--- TestRepeatAdditionQuiz.java
+++ TestRepeatAdditionQuiz.java
@@ -13,5 +13,19 @@ public class TestRepeatAdditionQuiz{
 		//print the question let the user enter the answer
 		System.out.print("What is " + repeat.getNumber1() + " x " + repeat.getNumber2() + " ? ");
 		int answer = input.nextInt();
+		
+		//if the answer is wrong then entire this loop
+		while(repeat.getAnswerCheck() != answer){
+			//add the wrong answer in the answerList(ArrayList)
+			repeat.setAnswerList(answer);
+			repeat.setAddAnswerList(answer);
 
+			//print the message to tell the user the answer is wrong
+			System.out.print("Wrong answer.  Try again. What is " + repeat.getNumber1() + " x " + repeat.getNumber2() + " ? ");
+			answer = input.nextInt();
+		}
+	
+		//if the answer is true then print the message answer to tell the user whose answer is true
+		System.out.println("You get it !");
+	}
 }
